dd6d88b067fb2c00f3b256c05a7068e5c38aaf3d
OksanaBer <berdovskaoksana@gmail.com>
2024-06-18 10:58:21 +0300

    Kichen_Yana_360

---
+++ data.js
@@ -0,0 +1,194 @@
+var APP_DATA = {
+  "scenes": [
+    {
+      "id": "0-panorama_1",
+      "name": "Panorama_1",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        },
+        {
+          "tileSize": 512,
+          "size": 4096
+        }
+      ],
+      "faceSize": 4096,
+      "initialViewParameters": {
+        "yaw": 0,
+        "pitch": 0,
+        "fov": 1.2752188746020257
+      },
+      "linkHotspots": [
+        {
+          "yaw": -0.047095793694921895,
+          "pitch": -0.28886954887397565,
+          "rotation": 0,
+          "target": "1-panorama_2"
+        },
+        {
+          "yaw": 0.5302027872520707,
+          "pitch": -0.4120680719180623,
+          "rotation": 0,
+          "target": "2-panorama_3"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": -0.038275999695940754,
+          "pitch": -0.13747208384195986,
+          "title": "Кухня",
+          "text": "Варіант 2"
+        },
+        {
+          "yaw": 0.5432542283045532,
+          "pitch": -0.2938306924751899,
+          "title": "Кухня",
+          "text": "Варіант 3"
+        }
+      ]
+    },
+    {
+      "id": "1-panorama_2",
+      "name": "Panorama_2",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        },
+        {
+          "tileSize": 512,
+          "size": 4096
+        }
+      ],
+      "faceSize": 4096,
+      "initialViewParameters": {
+        "yaw": 0,
+        "pitch": 0,
+        "fov": 1.2752188746020257
+      },
+      "linkHotspots": [
+        {
+          "yaw": 0.13178678284300815,
+          "pitch": -0.39058331541299474,
+          "rotation": 0,
+          "target": "0-panorama_1"
+        },
+        {
+          "yaw": 0.6231950460625164,
+          "pitch": -0.31882539670697696,
+          "rotation": 0,
+          "target": "2-panorama_3"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": 0.13757180044775552,
+          "pitch": -0.245857084017036,
+          "title": "Кухня",
+          "text": "Варіант 1"
+        },
+        {
+          "yaw": 0.6309205981393831,
+          "pitch": -0.19720373498986277,
+          "title": "Кухня",
+          "text": "Варіант 3"
+        }
+      ]
+    },
+    {
+      "id": "2-panorama_3",
+      "name": "Panorama_3",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        },
+        {
+          "tileSize": 512,
+          "size": 4096
+        }
+      ],
+      "faceSize": 4096,
+      "initialViewParameters": {
+        "yaw": 0,
+        "pitch": 0,
+        "fov": 1.2752188746020257
+      },
+      "linkHotspots": [
+        {
+          "yaw": -0.28375196696673655,
+          "pitch": -0.19270181472314718,
+          "rotation": 0,
+          "target": "0-panorama_1"
+        },
+        {
+          "yaw": 0.34238776750960476,
+          "pitch": -0.17843630389299037,
+          "rotation": 0,
+          "target": "1-panorama_2"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": -0.2945771003423978,
+          "pitch": -0.04788260253700827,
+          "title": "Кухня",
+          "text": "Варіант 1"
+        },
+        {
+          "yaw": 0.34238784683866186,
+          "pitch": -0.04713470869081959,
+          "title": "Кухня",
+          "text": "Варіант 2"
+        }
+      ]
+    }
+  ],
+  "name": "Project Title",
+  "settings": {
+    "mouseViewMode": "drag",
+    "autorotateEnabled": false,
+    "fullscreenButton": false,
+    "viewControlButtons": false
+  }
+};
